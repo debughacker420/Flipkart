f170e69008c155cf98ed0253dc3a3fe8a6accaed
Ritesh Kumar Sharma <system@Riteshs-MacBook-Air.local>
2026-03-29 20:06:22 +0530

    fix: switch to HashRouter to fix 404 page not found errors on refresh for Render/Static environments

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import store from './redux/store.js'
 import App from './App.jsx'
 import './index.css'
@@ -9,9 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
